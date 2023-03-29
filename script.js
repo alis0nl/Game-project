4bc9642ad7c74ee8a410ff4ff5b595b4bc785fce
alis0nl <110567079+alis0nl@users.noreply.github.com>
2023-03-29 10:40:16 -1000

    c

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
+//this is an object 
 function jumper() {
+    //define it's position because we are inside object, use "this", when outisde use "jumper"
     this.x = 0
-    this.y = 0 
-    this.gravity = .4
+    this.y = 0
+    this.gravity = .6 
+    //oppose gravity 
     this.lift = -20
-    this.velocity = 0 
+    //velocity 
+    this.velocity = 0
     this.width = 150
-    this.height = 150  
-}
+    this.height = 150
 
-this.show = function() {
-    image(papple,this.x,this.y,this.width,this.height,)
-}
-
-this.update = function() {
-    this.velocity += this.gravity 
-    this.y += this.velocity 
-    this.velocity *= 0.9 
-    if (this.y < 0) {
-        this.y = 0
+    // build a function to declare what I look like 
+    this.show = function() {
+        image(ninja,this.x,this.y,this.width,this.height)
     }
+    this.up = function(){
+        //take initial velocity and modify by opposing gravity 
+        this.velocity  += this.lift 
+    }
+    this.update = function() {
+        this.velocity += this.gravity 
+        this.y += this.velocity 
+        //air resistance 
+        this.velocity *= 0.9 
+        //stop jumper from leaving the screen 
+        if (this.y > h) {
+            this.y = h 
+            this.velocity = 0
+        }
+        if (this.y < 0 ) {
+            this.y = 0
+        }
     }
 
-
-
+}
