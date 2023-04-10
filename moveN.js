8a280aff53c8498344f5ba4888fdbbed87a7b4d6
alis0nl <110567079+alis0nl@users.noreply.github.com>
2023-04-09 23:11:35 -1000

    m

---
+++ moveN.js
@@ -0,0 +1,18 @@
+function moving() {
+    this.x = 0 
+    this.y = h/2 
+    this.gravity = .3 
+    this.lift = -20
+    this.velocity = 0
+    this.width = 120
+    this.height = 120
+
+    this.show = function(){
+        image(ninja,this.x,this.y,this.width,this.height)
+    }
+    this.up = function() {
+        this.velocity += this.lift
+    }
+    
+
+    }
--- script.js
+++ script.js
@@ -3,28 +3,23 @@ function jumper() {
     //define it's position because we are inside object, use "this", when outisde use "jumper"
     this.x = 0
     this.y = 0
-    this.gravity = .6 
+    this.gravity = .3
     //oppose gravity 
     this.lift = -20
     //velocity 
     this.velocity = 0
-    this.width = 150
-    this.height = 150
+    this.width = 80
+    this.height = 80
 
     // build a function to declare what I look like 
     this.show = function() {
-        image(ninja,this.x,this.y,this.width,this.height)
-    }
-    this.up = function(){
-        //take initial velocity and modify by opposing gravity 
-        this.velocity  += this.lift 
+        image(papple,this.x,this.y,this.width,this.height)
     }
     this.update = function() {
-        this.velocity += this.gravity 
-        this.y += this.velocity 
-        //air resistance 
-        this.velocity *= 0.9 
-        //stop jumper from leaving the screen 
+            this.velocity += this.gravity 
+            this.y += this.velocity 
+            //air resistance 
+            this.velocity *= 0.9 
         if (this.y > h) {
             this.y = h 
             this.velocity = 0
@@ -33,5 +28,9 @@ function jumper() {
             this.y = 0
         }
     }
-
 }
+
+
+
+
+
