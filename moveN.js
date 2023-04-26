506dd60d50826ada84d66b15fbcfdefb90c7c6ce
alis0nl <110567079+alis0nl@users.noreply.github.com>
2023-04-25 23:11:43 -1000

    r

--- moveN.js
+++ moveN.js
@@ -2,15 +2,16 @@ function moving() {
     this.width = 120
     this.height = 120
     this.x = 0 
-    this.y = h - this.height 
-    this.gravity = .3 
-    this.lift = -20
+    this.y = h - 200
+    this.gravity = .7 
+    this.lift = -2
     this.velocity = 0
     
 
     this.show = function() {
         image(ninja,this.x,this.y,this.width,this.height)
     }
+ 
 
     this.walk = function() {
         if (keyIsDown(RIGHT_ARROW)&&(this.x < w)) {
@@ -19,12 +20,27 @@ function moving() {
         if (keyIsDown(LEFT_ARROW) && (this.x > 0 )){
         this.x = this.x - 5;
     }
-        if (keyIsDown(DOWN_ARROW) && (this.y +30 < h)){
-        this.y = this.y + 5;
-    } 
+//change to keyispressed
         if (keyIsDown(UP_ARROW)&& (this.y > 0)){
-        this.y = this.y - 5;
+        //this.y = this.y - 5;  
+        //this.y += this.gravity
+            this.velocity += this.lift 
+         
+
     }
+    }
+
+    this.update = function() {
+        this.velocity += this.gravity 
+        this.y += this.velocity 
+        if (this.y + this.height > h) {
+            this.y = h - this.height 
+            this.velocity = 0 
+        }
+        if (this.y < 0) {
+            this.y = 0 - this.height 
+        }
+
     }
    
 
